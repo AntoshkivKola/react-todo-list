5c9c77a72f3070b0a7d1f949ac5cf9dd2d980b8c
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-03-09 20:01:46 +0200

    Refact validation schema

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -2,6 +2,6 @@ import * as Yup from 'yup';
 
 export const TODO_TASK_SCHEMA = Yup.object({
   newTask: Yup.string()
-    .matches(/^[a-z].{2,}$/, '2 letters or more')
+    .matches(/^[0-9]||[A-Z]||[a-z].{2,}$/, '2 letters or more')
     .required(),
 });
