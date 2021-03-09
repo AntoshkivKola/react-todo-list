6297e2b79bf9095bd26c0fc7bd2db6c81dada84f
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-03-09 20:02:18 +0200

    Add page styles

--- src/Pages/TodoList/index.jsx
+++ src/Pages/TodoList/index.jsx
@@ -1,22 +1,21 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styles from './TodoList.module.scss';
 import NewTodoTask from 'components/NewTodoTask';
 import TodoTasksList from 'components/TodoTasksList';
 import { TodoListContext } from 'context';
-
-// import { todoList } from 'todoList';
+import { tasks } from 'tasks';
 
 const TodoList = () => {
   const [todoList, setTodoList] = useState([]);
-  //const todoList = useContext(TodoListContext);
 
   return (
     <TodoListContext.Provider value={todoList}>
-      <div>
-        <NewTodoTask todoList={todoList}  setTodoList={setTodoList}/>
-
-        <TodoTasksList todoList={todoList} setTodoList={setTodoList} />
+      <div className={styles.main}>
+        <div className={styles.container}>
+          <NewTodoTask todoList={todoList} setTodoList={setTodoList} />
+          <TodoTasksList todoList={todoList} setTodoList={setTodoList} />
+        </div>
       </div>
     </TodoListContext.Provider>
   );
