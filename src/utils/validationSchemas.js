7a866bbdc3b9b1647a8ee2f96f22be65511f7e78
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-03-11 16:31:33 +0200

    Rafact regex

--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -2,6 +2,6 @@ import * as Yup from 'yup';
 
 export const TODO_TASK_SCHEMA = Yup.object({
   newTask: Yup.string()
-    .matches(/^[0-9]||[A-Z]||[a-z].{2,}$/, '2 letters or more')
+    .matches(/([^\s])[a-z]{2,}$/, '2 letters or more')
     .required(),
 });
